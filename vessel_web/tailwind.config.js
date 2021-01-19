649cb428e607706bcda8810b4b54453ab08b5e59
Lucas Declercq <lucas.declercq@ineat-conseil.fr>
2021-01-19 17:53:06 +0100

    chore(frontend): install tailwind css framework

---
+++ vessel_web/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: {
+    content: ['./public/index.html', './src/**/*.svelte'],
+    options: {
+      safelist: [/svelte-/],
+    },
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}

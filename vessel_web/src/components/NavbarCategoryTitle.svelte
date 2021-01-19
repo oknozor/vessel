1f2dc023440b377b08d49f2333053a1eca97976d
Lucas Declercq <lucas.declercq@ineat-conseil.fr>
2021-01-19 21:04:45 +0100

    refactor(frontend): convert navbar to tailwind

---
+++ vessel_web/src/components/NavbarCategoryTitle.svelte
@@ -0,0 +1,11 @@
+<script>
+    export let title;
+</script>
+
+<h3 class="min-w-0 pl-1 pr-1 font-normal leading-snug pl-2 mr-0 ml-0 mb-3 mt-8 text-sm">{title}</h3>
+
+<style>
+    h3 {
+        color: #181922;
+    }
+</style>

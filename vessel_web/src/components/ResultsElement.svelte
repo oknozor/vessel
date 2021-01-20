80235f8761c02d5c8b9185eb79af83de4a0e2a82
Lucas Declercq <lucas.declercq@ineat-conseil.fr>
2021-01-20 14:14:29 +0100

    feat(frontend): initialize result table

---
+++ vessel_web/src/components/ResultsElement.svelte
@@ -0,0 +1,47 @@
+<script>
+    export let username;
+    export let directory;
+    export let speed;
+    export let position;
+
+    let countries = [
+        'fr',
+        'gb',
+        'be',
+        'us',
+        'de'
+    ]
+
+    let randomCountry = countries[Math.floor(Math.random() * countries.length)]
+</script>
+
+<tr>
+    <td class="px-6 py-4 whitespace-nowrap">
+        <div class="flex items-center">
+            <div class="flex-shrink-0 h-10 w-10">
+                <img class="h-10 w-10 rounded-full"
+                     src="/assets/flags/{randomCountry}.png"
+                     alt="">
+            </div>
+            <div class="ml-4">
+                <span class="text-sm font-medium text-gray-900">
+                    {username}
+                </span>
+            </div>
+        </div>
+    </td>
+    <td class="px-6 py-4 whitespace-nowrap">
+        <span class="text-sm text-gray-900">{directory}</span>
+    </td>
+    <td class="px-6 py-4 whitespace-nowrap">
+                    <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                      {speed}
+                    </span>
+    </td>
+    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+        {position}
+    </td>
+    <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+        <a href="http://wikipedia.org" class="text-gray-900 hover:text-indigo-900"><svg height="12" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="chevron-right" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" class="svg-inline--fa fa-chevron-right fa-w-10 fa-2x"><path fill="currentColor" d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z" class=""></path></svg></a>
+    </td>
+</tr>

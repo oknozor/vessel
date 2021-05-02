f472135bc8be973ae5af0ff0f00295b9e75e7d56
Paul Delafosse <paul.delafosse@protonmail.com>
2021-05-02 17:37:50 +0200

    refactor: user url query param for search query

--- vessel_http/src/model/mod.rs
+++ vessel_http/src/model/mod.rs
@@ -1,6 +1,11 @@
 use serde_derive::{Deserialize, Serialize};
 
 #[derive(Deserialize, Serialize)]
+pub struct SearchQuery {
+    pub term: String,
+}
+
+#[derive(Deserialize, Serialize)]
 pub struct QueueRequest {
     pub(crate) filename: String,
 }
--- vessel_http/src/routes/search.rs
+++ vessel_http/src/routes/search.rs
@@ -1,18 +1,37 @@
 use warp::Filter;
 
 use soulseek_protocol::server::messages::request::ServerRequest;
-use soulseek_protocol::server::messages::search::SearchRequest;
+use soulseek_protocol::server::messages::search::{SearchRequest};
 
 use crate::sender::VesselSender;
+use crate::model::SearchQuery;
+use warp::http::StatusCode;
+use warp::http::Response;
+use std::collections::HashMap;
 
 pub fn search(
     sender: VesselSender<ServerRequest>,
 ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
-    warp::path!("search" / String).map(move |query| {
-        sender.send(ServerRequest::FileSearch(SearchRequest {
-            ticket: rand::random(),
-            query,
-        }));
-        "ok"
+    let opt_query = warp::query::<SearchQuery>()
+        .map(Some)
+        .or_else(|_| async { Ok::<(Option<SearchQuery>,), std::convert::Infallible>((None,)) });
+
+    warp::path!("search")
+        .and(opt_query)
+        .map(move |query: Option<SearchQuery>| {
+        match query {
+            Some(query) => {
+                sender.send(ServerRequest::FileSearch(SearchRequest {
+                    ticket: rand::random(),
+                    query: query.term,
+                }));
+                Response::builder()
+                    .status(StatusCode::NO_CONTENT)
+                    .body("ok")
+            }
+            None => Response::builder()
+                .status(StatusCode::BAD_REQUEST)
+                .body("Failed to decode query param."),
+        }
     })
 }
